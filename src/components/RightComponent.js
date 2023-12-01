0e2862e871d9b1950a8dac5f6c1beda295639d66
tannerdavison <tboydavison@gmail.com>
2023-12-01 03:53:56 -0700

    yabadoobadoo

--- src/components/RightComponent.js
+++ src/components/RightComponent.js
@@ -5,7 +5,7 @@ import styled from "styled-components";
 const RightComponent = ({episodes, score, scoredBy}) => {
     return (
 			<Wrapper>
-				<div className={'centeredDiv'}>
+				<div className={"centeredDiv"}>
 					All Episodes: <span>{episodes}</span>
 				</div>
 				<h3>
@@ -14,10 +14,12 @@ const RightComponent = ({episodes, score, scoredBy}) => {
 						<h1>Anime Score </h1>
 						<span> {score} / 10</span>
 						<h5>
-                        <div className={'scoredBy'}>
-                                        <em>OVER</em>
-                            <span>{scoredBy}</span>
-								Have scored this title so far.
+							<div className={"scoredBy"}>
+								<em>OVER</em>
+								<span>
+									{scoredBy} <em>people</em>
+								</span>
+								Have participated in the scoring of these results.
 							</div>
 						</h5>
 					</div>
@@ -56,18 +58,21 @@ const Wrapper = styled.div`
         justify-content: center;
     }
     .scoredBy{
-        display: flex;
-        flex-direction: column;
-        width:100px;
-        gap:0px;
-        align-items: center;
-        justify-content:center
+           display: flex;
+    flex-direction: column;
+    width: 166px;
+    border-radius:18px;
+    background: rgba(0,0,0,0.4);
+    gap: 0px;
+    align-items: center;
+    justify-content: center;
+}
     }
     .centeredDiv{
         position: relative;
         display:flex;
         align-items: center;
         justify-content: center;
-        
+
     }
 `;
