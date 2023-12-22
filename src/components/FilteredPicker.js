fb01189297f46e664cb169dd3707a6dc40f28369
tannerdavison <tboydavison@gmail.com>
2023-12-21 22:13:36 -0700

    added filter

--- src/components/FilteredPicker.js
+++ src/components/FilteredPicker.js
@@ -1,15 +1,43 @@
 import React from 'react'
+import { useState } from 'react';
 import styled from 'styled-components';
 
 const FilteredPicker = ({pageData}) => {
-  const typeOfAnime = pageData ? pageData.map((show) => show.type) : '';
-  // const filtered = pageData.filter((show)=>show.rating !== 'RX - Hentai' )
-  console.log(typeOfAnime)
+ const animeTypes = ['TV','Special','Movie','OVA','ONA',"Music"]
+  const [currentValue, setCurrentValue] = useState('All')
+  const handleChange = (e) => {
+    setCurrentValue(e.target.value);
+  }
+  useState(() => {
+    console.log(currentValue)
+  },[currentValue])
   return (
-    <div>
-      <h1>Hello</h1>
-    </div>
-  )
+		<Wrapper>
+			<label htmlFor='animeTypeList'>Filter</label>
+			<select
+        name='Types:'
+        id={"animeTypeList"}
+        onChange={(e)=>handleChange(e)}>
+        <option value='ALL'selected='All'>ALL</option>
+				{animeTypes.map((option) => {
+          return <option
+            key={option}
+            value={option}
+            >{option}</option>;
+				})}
+			</select>
+		</Wrapper>
+	);
 }
 
 export default FilteredPicker
+const Wrapper = styled.div`
+display:flex;
+flex-direction: row;
+align-items: center;
+gap:10px;
+
+label{
+  font-family: Archivo;
+}
+`
